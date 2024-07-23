40869f5a756fb5ae56e696b80ce97cc93cb76051
Bernice Choy <bcpz@Bernices-MacBook-Air.local>
2024-07-23 15:54:34 +0800

    comment both modules for user to uncomment explicitly

--- terraform_modules/aws/main.tf
+++ terraform_modules/aws/main.tf
@@ -1,22 +1,25 @@
 data "aws_caller_identity" "current" {}
 
-module "ApiGwS3Frontend" {
-  source = "./apigw_s3_openapi_body"
-  webname = "sp"
-  environment = "dev"
-  account_id = data.aws_caller_identity.current.account_id
-  region = "ap-southeast-1"
-  apigw_description = "[Terraform]Create a API Gateway RestApi for serving Single Page Application through OpenAPI specification`body` argument"
-  deployment_description = "Deployment v1.0"
-}
+# Uncomment one of the modules to observe its behavior when provisioning
 
 
-module "ApiGwS3Frontend" {
-  source = "./apigw_s3_tf_resources"
-  webname = "sp"
-  environment = "dev"
-  account_id = data.aws_caller_identity.current.account_id
-  region = "ap-southeast-1"
-  apigw_description = "[Terraform]Create a API Gateway RestApi for serving Single Page Application via Terraform resources"
-  deployment_description = "Deployment v1.0"
-}
+# module "ApiGwS3Frontend" {
+#   source = "./apigw_s3_openapi_body"
+#   webname = "sp"
+#   environment = "dev"
+#   account_id = data.aws_caller_identity.current.account_id
+#   region = "ap-southeast-1"
+#   apigw_description = "[Terraform]Create a API Gateway RestApi for serving Single Page Application through OpenAPI specification`body` argument"
+#   deployment_description = "Deployment v1.0"
+# }
+
+
+# module "ApiGwS3Frontend" {
+#   source = "./apigw_s3_tf_resources"
+#   webname = "sp"
+#   environment = "dev"
+#   account_id = data.aws_caller_identity.current.account_id
+#   region = "ap-southeast-1"
+#   apigw_description = "[Terraform]Create a API Gateway RestApi for serving Single Page Application via Terraform resources"
+#   deployment_description = "Deployment v1.0"
+# }
